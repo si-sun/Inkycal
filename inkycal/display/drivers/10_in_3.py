3eb3686caf0a1b53c1daa0e423fe4d75580b8d8b
Ace <saadnaseer63@yahoo.de>
2021-11-29 13:40:46 +0100

    experimenting with 10.3" display support

---
+++ inkycal/display/drivers/10_in_3.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+10.3" driver class
+Copyright by aceisace
+"""
+from inkycal.custom import images, top_level
+from os.path import exists
+from PIL import Image
+
+# Display resolution
+EPD_WIDTH       = 1872
+EPD_HEIGHT      = 1404
+
+driver_dir = top_level+'/inkycal/display/drivers/10_in_3_drivers/'
+
+class EPD:
+
+  def __init__(self):
+    """10.3" epaper class"""
+    # Check if zipped folders are present, if yes, assume
+    # drivers have not been installed yet
+
+    if exists(f'{driver_dir}IT8951.zip'):
+      print('Additional steps are required to install drivers for 10.3" E-Paper. '
+            'Please run the following command in Terminal, then retry:\n'
+            f'bash {driver_dir}install.sh')
+
+  def init(self):
+    pass
+
+  def display(self, command):
+    """displays an image"""
+    try:
+      run_command = command.split()
+      run(run_command)
+    except:
+      print("oops, something didn't work right :/")
+
+  def getbuffer(self, image):
+    """ad-hoc"""
+    image = image.rotate(90, expand=True)
+    image.convert('RGB').save(images+'canvas.bmp', 'BMP')
+    command = f'sudo {driver_dir}IT8951/IT8951 0 0 {images+"canvas.bmp"}'
+    print(command)
+    return command
+
+  def sleep(self):
+    pass
+
